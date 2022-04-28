0b76eabaed979da55be6efb8ec46780d86dfc920
Ramath <ramathmanjitha@gmail.com>
2022-04-28 21:50:54 +0530

    sample

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import cors from 'cors';
 import 'dotenv/config' ;
 
 const app = express();
-const PORT = process.env.PORT || "8090";
+const PORT = process.env.PORT || "8091";
 
 app.use(cors());
 app.use(express.json({limit: '20mb'}));
 
 app.get("/", (req, res, next) =>{
-    res.send("<h2>Library Managment System</h2>")
+    res.send("<h2>Library Managment System</h2>");
+    next();
 })
 
 app.listen(PORT, ()=>{
